73cd5167bb3b0b76bfffb40d2ccfab8fb4eee4d5
trachpro <tu.phamminh.2207@gmail.com>
2018-04-15 16:50:20 +0700

    ád

--- src/app/core/config.service.ts
+++ src/app/core/config.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ConfigService {
 
-  private urlBase = 'https://http://app-40dcc8a9-fda5-499f-8e36-8c8e8efe51d2.cleverapps.io/api/'
+  private urlBase = 'https://app-40dcc8a9-fda5-499f-8e36-8c8e8efe51d2.cleverapps.io/api/'
   constructor() { }
 
   getBaseURL() {
